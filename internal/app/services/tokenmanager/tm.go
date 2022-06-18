96de779d5b66aaf7c40a83e6b954318a741a929b
Yakimkin Roman <r.yakimkin@yandex.ru>
2022-06-18 13:03:39 +0300

    Add refresh token forbidden list to logout.
    Add refresh token forbidden list to /logout.

--- internal/app/services/tokenmanager/tm.go
+++ internal/app/services/tokenmanager/tm.go
@@ -5,6 +5,6 @@ import "auth/internal/app/models"
 type TokenManager interface {
 	GenerateAccessToken(u *models.User) (string, error)
 	GenerateRefreshToken(u *models.User) (string, error)
-	ParseAccessToken(token string) (string, error)
-	ParseRefreshToken(token string) (string, error)
+	ParseAccessToken(token string) (*AccessClaims, error)
+	ParseRefreshToken(token string) (*RefreshClaims, error)
 }
